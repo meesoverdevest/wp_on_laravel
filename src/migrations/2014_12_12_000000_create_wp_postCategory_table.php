f1e91a162bd3607425c2e83236e7d06f1cda042e
biertjesman <mees@mefolio.nl>
2017-03-07 21:38:33 +0000

    Rename migration files

--- src/migrations/2014_12_12_000000_create_wp_postCategory_table.php
+++ src/migrations/2014_12_12_000000_create_wp_postCategory_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostCategoryTable extends Migration
+class CreateWpPostCategoryTable extends Migration
 {
     /**
      * Run the migrations.
